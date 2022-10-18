ba641c8a3f1589b31e9592f460fe923614772786
Joe Wang <joewa@google.com>
2022-10-18 13:44:13 -0400

    reduce beans

--- src/main/java/com/example/gsmtest/code/DataSourceConfig.java
+++ src/main/java/com/example/gsmtest/code/DataSourceConfig.java
@@ -2,13 +2,11 @@ package com.example.gsmtest.code;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
 
 @Component
 public class DataSourceConfig {
@@ -16,23 +14,13 @@ public class DataSourceConfig {
     @Value("${application.secret}")
     private String csql;
 
-    @Bean
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSourceProperties getDatasourceProperties() {
-        return new DataSourceProperties();
-    }
-
     @Bean(name = "masterDataSource")
-    public DataSource dataSource() throws JsonProcessingException {
-
+    public DataSource dataSource(DataSourceProperties properties) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
-
         CSQL csqlData = objectMapper.readValue(csql, CSQL.class);
-
-        DataSource dataSource =  getDatasourceProperties().initializeDataSourceBuilder()
+        return properties.initializeDataSourceBuilder()
                 .username(csqlData.username)
                 .password(csqlData.password)
                 .build();
-        return dataSource;
     }
 }
